e54f3c86f226f694869c958dee2ef04ffd22a81a
eugen <eschlecht119@googlemail.com>
2017-10-13 11:06:26 +0200

    Teilnehmer class added

---
+++ src/main/java/model/Teilnehmer.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Teilnehmer {
+
+
+    public String bereichString;
+
+    public String getBereichString() {
+        return bereichString;
+    }
+
+    public void setBereichString(String bereichString) {
+        this.bereichString = bereichString;
+    }
+
+    public String meldergruppeString;
+
+    public String getMeldergruppeString() {
+        return meldergruppeString;
+    }
+
+    public void setMeldergruppeString(String meldergruppeString) {
+        this.meldergruppeString = meldergruppeString;
+    }
+
+    public String meldergruppenNameString;
+    public String melderpunktNummer;
+    public String teilnehmer;
+
+
+}
